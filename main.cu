e0b31dc5823c8f1f159f3d5c7f8e92f4dddcdc69
Ryuta339 <soccer.271828.339@gmail.com>
2023-04-02 04:32:19 +0900

    feat: class template

--- main.cu
+++ main.cu
@@ -2,6 +2,49 @@
 #include <stdlib.h>
 #include "cuda_runtime.h"
 
+template<class T>
+class Doubler {
+	T obj;
+	public:
+		void vecDouble(int *in, int *out, const size_t n) {
+			obj.vecDouble(in, out, n);
+		}
+};
+
+class HostDoubler {
+	public:
+		void vecDouble(int *in, int *out, const size_t n) {
+			for (size_t i=0; i<n; i++)
+				out[i] = in[i] * 2;
+		}
+}
+
+class DeviceDoubler {
+	private:
+		__global__ void kernel_vecDouble(int *in, int *out, const size_t n) {
+			int i = threadIdx.x;
+			if (i < n) {
+				out[i] = in[i] * 2;
+			}
+		}
+
+	public:
+		static void vecDouble(int *hIn, int *hOut, const size_t n) {
+			int *dIn;
+			int *dOut;
+			cudaMalloc((void **)&dIn, n * sizeof(int));
+			cudaMalloc((void **)&dOut, n * sizeof(int));
+			cudaMemcpy(dIn, hIn, n * sizeof(int), cudaMemcpyHostToDevice);
+
+			kernel_vecDouble<<<1, n>>>(dIn, dOut, n);
+			cudaDeviceSynchronize();
+
+			cudaMemcpy(hOut, dOut, n * sizeof(int), cudaMemcpyDeviceToHost);
+			cudaFree(dIn);
+			cudaFree(dOut);
+		}
+}
+
 static void vecDouble(int *, int *, const size_t);
 
 int main() {
@@ -14,10 +57,12 @@ int main() {
 
 	for (size_t i=0; i<n; i++)
 		in[i] = rand() % 100;
-	for (size_t i=0; i<n; i++)
-		answer[i] = in[i] * 2;
 
-	vecDouble(in, out, n);
+	Doubler<HostDoubler> host;
+	Doubler<DeviceDoubler> device;
+
+	host.vecDouble(in, answer, n);
+	device.vecDouble(in, out, n);
 
 	for (size_t i=0; i<n; i++) {
 		if (answer[i] != out[i]) {
@@ -32,26 +77,4 @@ int main() {
 	delete[] answer;
 
 	return 0;
-}
-
-__global__ void kernel_vecDouble(int *in, int *out, const size_t n) {
-	int i = threadIdx.x;
-	if (i < n) {
-		out[i] = in[i] * 2;
-	}
-}
-
-static void vecDouble(int *hIn, int *hOut, const size_t n) {
-	int *dIn;
-	int *dOut;
-	cudaMalloc((void **)&dIn, n * sizeof(int));
-	cudaMalloc((void **)&dOut, n * sizeof(int));
-	cudaMemcpy(dIn, hIn, n * sizeof(int), cudaMemcpyHostToDevice);
-
-	kernel_vecDouble<<<1, n>>>(dIn, dOut, n);
-	cudaDeviceSynchronize();
-
-	cudaMemcpy(hOut, dOut, n * sizeof(int), cudaMemcpyDeviceToHost);
-	cudaFree(dIn);
-	cudaFree(dOut);
 }
